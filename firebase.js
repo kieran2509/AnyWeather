c8acde6681c3a36641921c9fa61170c7abcabd81
Kieran Bailey <k.bailey@students.rave.ac.uk>
2017-03-06 14:38:51 +0000

    trial

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var database = firebase.database();
+var placesDatabase = database.ref('places');
+var placesList = [];
+
+placesDatabase.on('child_added', function( firebaseObject ) 
+{
+    var place = firebaseObject.val(); 
+    placesList.push(person);
+    // "push" is JavaScript's lingo for "add to a list"
+})
+
+console.log("placeslist")
