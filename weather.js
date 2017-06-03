6a9149771658f6166ce476ed343805a124990669
Kieran Bailey <k.bailey@students.rave.ac.uk>
2017-06-03 23:01:02 +0100

    using dark sky instead of open weather so that it will remain secure

--- weather.js
+++ weather.js
@@ -1,6 +1,6 @@
 navigator.geolocation.watchPosition(gotPosition)
 
-var APIKey = 'a120aa36dac8f8043805f63a51019310'
+var APIKey = 'ea549adf416040caa38e5e62cacbf798'
 
 function gotPosition(position) 
 {   
@@ -9,7 +9,7 @@ function gotPosition(position)
 
 function getTheWeather(position)
 {
-    var URL = 'http://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&APPID=' + APIKey
+    var URL = 'https://api.darksky.net/forecast/'+APIKey+'/'+position.coords.latitude+','+position.coords.longitude
     console.log(URL)
 
     $.ajax(
@@ -27,110 +27,45 @@ function handleData(json)
 {
     console.log(json)
 
-    weather = json.weather[0]
-        
-    window.weatherid = weather.id
+    weather = json.currently.icon
+    console.log(weather)    
+
+    window.weatherid = weather
     //    new Number(weatherid)  
     console.log(weatherid)
 
 console.log(weatherid)
 
 switch(weatherid) {
-        case 200:
-        case 201:
-        case 202:
-        case 210:
-        case 211:
-        case 212:
-        case 221:
-        case 230:
-        case 231:
-        case 232:
-        case 711:
-        case 731:
-        case 771:
-        case 751:
-        case 761:
-        case 762:
-        case 771:
-        case 781:
-        case 900:
-        case 901:
-        case 902:
-        case 957:
-        case 958:
-        case 959:
-        case 960:
-        case 961:
-        case 962:
+        case 'hail':
+        case 'tornado':
+        case 'thunderstorm':
+        case 'wind':
             console.log ("Stormy");
             currentWeather = 'storm' 
             break;
-        case 301:
-        case 302:
-        case 310:
-        case 311:
-        case 312:
-        case 313:
-        case 314:
-        case 321:
-        case 500:
-        case 501:
-        case 502:
-        case 503:
-        case 504:
-        case 511:
-        case 520:
-        case 522:
-        case 531:
-        case 701:
-        case 906:
+        case 'rain':
             console.log ("Raining");
             currentWeather = 'rain' 
             break;
-        case 600:
-        case 601:
-        case 602:
-        case 611:
-        case 612:
-        case 615:
-        case 616:
-        case 620:
-        case 621:
-        case 622:
+        case 'snow':
+        case 'sleet':
             console.log ("Snowy");
             currentWeather = 'snow' 
             break;
-        case 800:
-        case 801:
-        case 951:
-        case 952:
-        case 953:
-        case 954:
-        case 955:
-        case 956:
-        case 721:
+        case 'clear-day':
+        case 'clear-night':
             console.log ("Sunny");
             currentWeather = 'sun';
             console.log(currentWeather)
             break;
-        case 300:
-        case 741:
-        case 802:
-        case 803:
-        case 804:
-        case 521:
+        case 'fog':
+        case 'cloudy':
+        case 'partly-cloudy-day':
+        case 'partly-cloudy-night':
             console.log ("Cloudy");
             currentWeather = 'cloud'
             break;
-        case 904:
-            console.log ("Hot day");
-            currentWeather = 'hot_day'
-            break;
-        case 903:
-        case 905:
-            console.log ("Cold day");
-            currentWeather = 'cold_day'
 }
 var event = new Event('weatherLoaded');
     window.dispatchEvent(event);
